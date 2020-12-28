a5339551891b7372e3889dce0c2eb46b3cdfe940
jinlow <james.d.inlow@gmail.com>
2020-12-28 10:56:37 -0600

    Adding regex arg search

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{App, Arg};
 use crossterm::Result;
+use regex::{Regex, Match};
 use std::time;
 
 mod timechunk;
@@ -62,13 +63,47 @@ fn main() -> Result<()> {
         )
         .get_matches();
 
-    let time_vals: Vec<&str> = tm.values_of("time").unwrap_or_default().collect();
+    // Parse out time values
+    if tm.is_present("time") {
+        // Create the hours, minutes and seconds regular expressions we will
+        // use to search the time values of.
+        let re_h = Regex::new(r"(\d+)h").unwrap();
+        let re_m = Regex::new(r"(\d+)m").unwrap();
+        let re_s = Regex::new(r"(\d+)s").unwrap();
 
-    if time_vals.len() > 0 {
+        let time_vals: Vec<&str> = tm.values_of("time").unwrap_or_default().collect();
         println!("greater than 0");
-        println!("{:?}", time_vals);
-        return Ok(());
+        println!("{:?}", &time_vals);
+        let hours = get_time_arg(&time_vals, re_h);
+        let minutes = get_time_arg(&time_vals, re_m);
+        let seconds = get_time_arg(&time_vals, re_s);
+        println!("{}: {}: {}", hours, minutes, seconds);
+        
     }
 
+    let re_h = Regex::new(r"(\d+)h").unwrap();
+    let test_text = "10h";
+    println!("{}", re_h.is_match(test_text));
+    println!(
+        "{}",
+        re_h.captures(test_text).unwrap().get(1).unwrap().as_str()
+    );
+
     Ok(())
+}
+
+// Function for parsing out the matched value.
+fn get_time_arg(time_matches: &Vec<&str>, search_re: Regex) -> u64 {
+    let val_match = time_matches.iter().find(|&&a| search_re.is_match(a));
+    let match_str = match val_match {
+        // We know we can unwrap here because of None is not returned we have captured
+        // at least 1 group.
+        Some(m) => search_re.captures(m).unwrap().get(1).unwrap().as_str(),
+        None => "0"
+    };
+
+    match match_str.parse::<u64>() {
+        Ok(t) => t,
+        _ => 0
+    }
 }
